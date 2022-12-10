092a66fac65abb0eac578a3fb75090a14efb5cb0
Ainis Taranda <taranda.ainis@gmail.com>
2022-12-09 21:45:04 -0500

    added user

--- functions/src/users.js
+++ functions/src/users.js
@@ -5,11 +5,100 @@ export async function addNewUser(req, res) {
   const newUser = req.body;
 
   const db = dbConnect();
-  const collection = await db.collection("program").find().toArray();
+  const programCollection = await db.collection("program").find().toArray();
+  newUser.days=programCollection
 
-  collection.forEach((program) => {
-    newUser[program.day] = program.movements;
-  });
+  // programCollection.forEach((program) => {
+  //     newUser.days =[{"day": 1, movements: program.movements}, {"day": 2, movements: program.movements},
+  //     {"day": 3, movements: program.movements}];
+  // });
+
+  // newUser.days =  [
+  //   {
+  //     "day": "1",
+  //     "movements": [
+  //       {
+  //         "movement": "Squat Variation",
+  //         "exercise": [
+  //           {
+  //             "name": "Back Squat",
+  //             "sets": "3",
+  //             "reps": "5",
+  //             "weight": "11111",
+  //             "rpe": "8"
+  //           },
+  //           {
+  //             "name": "Front Squat",
+  //             "sets": "3",
+  //             "reps": "5t",
+  //             "weight": "",
+  //             "rpe": "8"
+  //           },
+  //           {
+  //             "name": "Goblet Squat",
+  //             "sets": "3",
+  //             "reps": "5y",
+  //             "weight": "",
+  //             "rpe": "8"
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         "movement": "Deadlift Variation",
+  //         "exercise": [
+  //           {
+  //             "name": "Conventional Deadlift",
+  //             "sets": "3",
+  //             "reps": "5",
+  //             "weight": "2222",
+  //             "rpe": "8"
+  //           },
+  //           {
+  //             "name": "Romanian Deadlift",
+  //             "sets": "3",
+  //             "reps": "5",
+  //             "weight": "",
+  //             "rpe": "8"
+  //           },
+  //           {
+  //             "name": "Good Mornings",
+  //             "sets": "3",
+  //             "reps": "5",
+  //             "weight": "",
+  //             "rpe": "8"
+  //           }
+  //         ]
+  //       },
+  //       {
+  //         "movement": "Single Leg Variation",
+  //         "exercise": [
+  //           {
+  //             "name": "Bulgarian Split Squat",
+  //             "sets": "3",
+  //             "reps": "5",
+  //             "weight": "",
+  //             "rpe": "8"
+  //           },
+  //           {
+  //             "name": "Lunges",
+  //             "sets": "3",
+  //             "reps": "5",
+  //             "weight": "",
+  //             "rpe": "8"
+  //           },
+  //           {
+  //             "name": "Step Ups",
+  //             "sets": "3",
+  //             "reps": "5",
+  //             "weight": "",
+  //             "rpe": "8"
+  //           }
+  //         ]
+  //       }
+  //     ]
+  //   }
+  // ]
+  
   await db
     .collection("users")
     .insertOne(newUser)
@@ -22,10 +111,10 @@ export async function addNewUser(req, res) {
     .collection("users")
     .find({ email: newUser.email, password: newUser.password })
     .toArray();
+    
+  delete matchingUsers[0].password;
 
-    delete matchingUsers[0].password;
-
-    res.send(matchingUsers[0])
+  res.send(matchingUsers[0]);
 }
 
 export async function getAllUsers(req, res) {
@@ -47,15 +136,21 @@ export async function getOneUser(req, res) {
 export async function updateUser(req, res) {
   const db = dbConnect();
   const { userId } = req.params;
-
+  // const collection = await db
+  //   .collection("users")
+  //   .find({ _id: new ObjectId(userId) })
+  //   .toArray();
+  let body = req.body
+  delete body._id 
   await db
     .collection("users")
     .findOneAndUpdate({ _id: new ObjectId(userId) }, { $set: req.body })
     .catch((err) => {
-      res.status(500).send(err);
-      return;
+      // res.status(500).send(err);
+      // return;
+      console.error(err)
     });
-  res.status(202).send({ message: "user updated" });
+  res.status(202).send({ message: "user updated" })
 }
 
 export async function userLogin(req, res) {
